dffa3a69ba1195df4be549208c9fc629fbea3f4e
TouseefAbid <touseefabid737@gmail.com>
2024-08-20 12:55:27 +0500

    config "image-domain" change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['images.unsplash.com'], // Add other domains as needed
+      remotePatterns: [
+        {
+          protocol: 'https', // or 'http' if that's how your images are served
+          hostname: 'image.domain', // Replace 'image.domain' with your actual image domain
+          port: '', // Specify the port if it's not the default (80 for HTTP, 443 for HTTPS)
+          pathname: '/path-to-images/**', // Define the path or use '**' to match any path
+        },
+      ],
     },
   };
 
